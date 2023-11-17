7b742a2ea79f5f393951871323a35a400038a1f5
savageafrika <113559289+savageafrika@users.noreply.github.com>
2023-11-17 03:43:54 +0100

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,179 @@
+
+
+
+
+ 
+
+   //Questions array
+
+   let questions = [
+
+    {
+
+         number:1,
+
+         question: "__ is a term that is used to describe if a verb is active or passive",
+
+         answer : "Voice",
+
+         options : [
+
+                          "Voice",
+
+                          "Form",
+
+                          "Construction",
+
+                          "Tone"
+
+                        ]
+
+    
+
+    },
+
+    {
+
+         number:2,
+
+         question: "The thief was caught by the police man. This sentence is in __ form",
+
+         answer : "Passive",
+
+         options : [
+
+                          "Active",
+
+                          "Passive",
+
+                          "None of the above",
+
+                          "Both active and passive"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:3,
+
+         question: "__ type of verbs can be passivized",
+
+         answer : "Transitive verbs",
+
+         options : [
+
+                          "Static verbs",
+
+                          "Finite verbs",
+
+                          "Copular verbs",
+
+                          "Transitive verbs"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:4,
+
+         question: "When there are two objects of an active sentence, which of them takes the place of the subject in the passive form",
+
+         answer : "Indirect object",
+
+         options : [
+
+                          "Direct object",
+
+                          "Indirect object",
+
+                          "Object complement",
+
+                          "Object of the preposition"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:5,
+
+         question: "Choose the correct passive form of these sentence - Someone broke the vase",
+
+         answer : "The vase was broken",
+
+         options : [
+
+                          "The vase has been broken",
+
+                          "The vase was broken",
+
+                          "The vase has been broken by someone",
+
+                          "The vase was broken by someone"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:6,
+
+         question: "Choose the correct passive form of these sentence - The girls gave him a flower",
+
+         answer : "He was given a flower by girls",
+
+         options : [
+
+                          "He received the flower from the girls",
+
+                          "He has been given a flower",
+
+                          "A flower has been given to him by the girls",
+
+                          "He was given a flower by girls"  
+
+                          ]
+
+    
+
+    },
+
+    {
+
+         number:7,
+
+         question: "Choose the correct passive form of these sentence - Shut the door",
+
+         answer : "Have the door shut",
+
+         options : [
+
+                          "The door has been shut",
+
+                          "Have the door shut",
+
+                          "The door was been shut",
+
+                          "The door is to be shut"  
+
+                          ]
+
+    
+
+    }
+
+     ];   
